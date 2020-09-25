c26faa4aa31cbc63a256e3db25d24cbefe580e17
arunkumarsa7 <arunkumarsa8374@gmail.com>
2020-09-25 16:21:32 +0530

    Initial correction in files for proper working

--- src/main/java/com/ensat/services/ProductServiceImpl.java
+++ src/main/java/com/ensat/services/ProductServiceImpl.java
@@ -1,9 +1,12 @@
 package com.ensat.services;
 
-import com.ensat.entities.Product;
-import com.ensat.repositories.ProductRepository;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ensat.entities.Product;
+import com.ensat.repositories.ProductRepository;
 
 /**
  * Product service implement.
@@ -11,31 +14,32 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProductServiceImpl implements ProductService {
 
-    private ProductRepository productRepository;
+	private ProductRepository productRepository;
 
-    @Autowired
-    public void setProductRepository(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
+	@Autowired
+	public void setProductRepository(final ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
 
-    @Override
-    public Iterable<Product> listAllProducts() {
-        return productRepository.findAll();
-    }
+	@Override
+	public Iterable<Product> listAllProducts() {
+		return productRepository.findAll();
+	}
 
-    @Override
-    public Product getProductById(Integer id) {
-        return productRepository.findOne(id);
-    }
+	@Override
+	public Product getProductById(final Integer id) {
+		final Optional<Product> product = productRepository.findById(id);
+		return product.isPresent() ? product.get() : new Product();
+	}
 
-    @Override
-    public Product saveProduct(Product product) {
-        return productRepository.save(product);
-    }
+	@Override
+	public Product saveProduct(final Product product) {
+		return productRepository.save(product);
+	}
 
-    @Override
-    public void deleteProduct(Integer id) {
-        productRepository.delete(id);
-    }
+	@Override
+	public void deleteProduct(final Integer id) {
+		productRepository.deleteById(id);
+	}
 
 }
