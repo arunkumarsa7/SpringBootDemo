cc655f80fda7ebef5cb16b41c9365b5776ddbc2a
arunkumarsa7 <arunkumarsa8374@gmail.com>
2020-10-07 16:53:07 +0530

    Small changes

--- src/test/java/com/spring_boot/ListProductTests.java
+++ src/test/java/com/spring_boot/ListProductTests.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.spring_boot.SpringBootWebApplication;
 import com.spring_boot.controllers.ProductController;
 import com.spring_boot.entities.Product;
 
